edc27410398a32309fffd8b828fce675e7689288
shinefine <shinefine@gmail.com>
2015-08-11 22:57:10 +0800

    add admin assets

---
+++ public/admin/assets/global/js/pages/dashboard.js
@@ -0,0 +1,126 @@
+$(function() {
+
+
+
+
+    $(document).on("click", ".theme-color", function(event) {
+        var color = $(this).data('color');
+
+        $('#listdiv').mCustomScrollbar({
+            scrollButtons: {
+                enable: false
+            },
+            autoHideScrollbar: false,
+            scrollInertia: 150,
+            theme: "light-thin",
+            set_height: 440,
+            advanced: {
+                updateOnContentResize: true
+            }
+        });
+    });
+
+
+
+    /* Notifications, demo purpose */
+    notifContent = '<div class="alert alert-dark media fade in bd-0" id="message-alert"><div class="media-left"><img src="../assets/global/images/profil_page/friend8.jpg" class="dis-block img-circle"></div><div class="media-body width-100p"><h4 class="alert-title f-14">New message received</h4><p class="f-12 alert-message pull-left">John send you a message 2 hours ago.</p><p class="pull-right"><a href="#" class="f-12">Read message</a></p></div></div>';
+    setTimeout(function() {
+        if (!$('#quickview-sidebar').hasClass('open') && !$('.page-content').hasClass('page-builder') && !$('.morphsearch').hasClass('open')) generateNotifDashboard(notifContent);
+    }, 3000);
+
+
+    $('*[data-jquery-clock]').each(function() {
+        var t = $(this);
+        var seconds = new Date().getSeconds(),
+            hours = new Date().getHours(),
+            mins = new Date().getMinutes(),
+            sdegree = seconds * 6,
+            hdegree = hours * 30 + (mins / 2),
+            mdegree = mins * 6;
+        var updateWatch = function() {
+            sdegree += 6;
+            if (sdegree % 360 == 0) {
+                mdegree += 6;
+            }
+            hdegree += (0.1 / 12);
+            var srotate = "rotate(" + sdegree + "deg)",
+                hrotate = "rotate(" + hdegree + "deg)",
+                mrotate = "rotate(" + mdegree + "deg)";
+            $(".jquery-clock-sec", t).css({
+                "-moz-transform": srotate,
+                "-webkit-transform": srotate,
+                '-ms-transform': srotate
+            });
+            $(".jquery-clock-hour", t).css({
+                "-moz-transform": hrotate,
+                "-webkit-transform": hrotate,
+                '-ms-transform': hrotate
+            });
+            $(".jquery-clock-min", t).css({
+                "-moz-transform": mrotate,
+                "-webkit-transform": mrotate,
+                '-ms-transform': mrotate
+            });
+        }
+        updateWatch();
+        setInterval(function() {
+            $(".jquery-clock-sec, .jquery-clock-hour, .jquery-clock-min").addClass('jquery-clock-transitions');
+            updateWatch();
+        }, 1000);
+        $(window).focus(function() {
+            $(".jquery-clock-sec, .jquery-clock-hour, .jquery-clock-min").addClass('jquery-clock-transitions');
+        });
+        $(window).blur(function() {
+            $(".jquery-clock-sec, .jquery-clock-hour, .jquery-clock-min").removeClass('jquery-clock-transitions');
+        });
+    });
+
+
+
+    /* Progress Bar  Widget */
+    if ($('.widget-progress-bar').length) {
+        $(window).load(function() {
+            setTimeout(function() {
+                $('.widget-progress-bar .stat1').progressbar();
+            }, 900);
+            setTimeout(function() {
+                $('.widget-progress-bar .stat2').progressbar();
+            }, 1200);
+            setTimeout(function() {
+                $('.widget-progress-bar .stat3').progressbar();
+            }, 1500);
+            setTimeout(function() {
+                $('.widget-progress-bar .stat4').progressbar();
+            }, 1800);
+        });
+    };
+
+   
+    
+});
+
+function generateNotifDashboard(content) {
+    var position = 'topRight';
+    if ($('body').hasClass('rtl')) position = 'topLeft';
+    var n = noty({
+        text: content,
+        type: 'success',
+        layout: position,
+        theme: 'made',
+        animation: {
+            open: 'animated bounceIn',
+            close: 'animated bounceOut'
+        },
+        timeout: 4500,
+        callback: {
+            onShow: function() {
+                $('#noty_topRight_layout_container, .noty_container_type_success').css('width', 350).css('bottom', 10);
+            },
+            onCloseClick: function() {
+                setTimeout(function() {
+                    $('#quickview-sidebar').addClass('open');
+                }, 500)
+            }
+        }
+    });
+}
